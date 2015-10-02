48347046e0787303161fb2715f2033238ca8ed5a
Israel <israel.lewis@mnscorp.net>
2015-10-02 15:49:05 +0100

    Remove Build folder

---
+++ FeedMe/RestApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  RestApiManager.swift
+//  FeedMe
+//
+//  Created by Izureru on 02/10/2015.
+//  Copyright (c) 2015 Marks and Spencer. All rights reserved.
+//
+
+import Foundation
---
+++ FeedMe/SwiftyJSON.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftyJSON.swift
+//  FeedMe
+//
+//  Created by Izureru on 02/10/2015.
+//  Copyright (c) 2015 Marks and Spencer. All rights reserved.
+//
+
+import Foundation
